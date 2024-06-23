2f5781c583fcf4abfe58e3a0488f7bc20276b401
BothinaMesalem <bothinamesalem@gmail.com>
2024-06-23 16:53:51 +0300

    fixswal

--- angular/src/app/guest/no-guests/no-guests.component.ts
+++ angular/src/app/guest/no-guests/no-guests.component.ts
@@ -8,8 +8,7 @@ import { template } from 'lodash-es';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { FormsModule, FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from "../../../shared/shared.module";
-
-
+import swal from 'sweetalert2';
 
 @Component({
     selector: 'app-no-guests',
@@ -125,7 +124,7 @@ export class NoGuestsComponent {
   // }
 
   promptFileSelection(): void {
-    Swal.fire({
+    swal.fire({
       title: 'Enter the Name, Phone number, and Email. Each one in a column',
       icon: 'warning',
       showCancelButton: true,
