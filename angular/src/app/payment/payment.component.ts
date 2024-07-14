9cab449f6c7369621c4e92585c5159cdb85f9a8f
asmaa-elfatayry <asmaa.elfatayry22@gmail.com>
2024-07-15 01:45:39 +0300

    payment style

--- angular/src/app/payment/payment.component.ts
+++ angular/src/app/payment/payment.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { loadStripe } from '@stripe/stripe-js';
+
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrls: ['./payment.component.css']
 })
-export class PaymentComponent implements OnInit  {
+export class PaymentComponent implements OnInit {
   stripe: any;
   elements: any;
   card: any;
@@ -15,7 +16,30 @@ export class PaymentComponent implements OnInit  {
 
   async ngOnInit() {
     this.stripe = await loadStripe('pk_test_51PbhokHI9E95Zl9vzuPBiZV7E7SpMsqrFIFBLgX8tkfvC57mkWUUM3x6kDc8XkHjr8MlI6yydbrcQUoGl6Hdj68Z00Ble9w77m');
-    this.elements = this.stripe.elements();
+
+    const appearance = {
+      theme: 'stripe',
+      variables: {
+        colorPrimary: '#ffffff',
+        colorBackground: '#000000',
+        colorText: '#ffffff',
+        colorDanger: '#ff0000',
+      },
+      rules: {
+        '.Input': {
+          borderColor: 'hsl(279, 6%, 55%)',
+          borderRadius: '4px',
+          color: 'hsl(0, 0%, 100%)',
+        },
+        '.Input:focus': {
+          borderImageSource: 'linear-gradient(to right, hsl(249, 99%, 64%), hsl(278, 94%, 30%))',
+          borderImageSlice: '1',
+          outline: 'none',
+        },
+      }
+    };
+
+    this.elements = this.stripe.elements({ appearance });
     this.card = this.elements.create('card');
     this.card.mount('#card-element');
   }
@@ -42,5 +66,4 @@ export class PaymentComponent implements OnInit  {
     // });
     alert('Payment successful!');
   }
-
 }
