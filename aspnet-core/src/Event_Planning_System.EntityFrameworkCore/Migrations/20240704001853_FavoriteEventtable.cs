8b40bcfea89c0deca18fb141f5ceb021cb12cce8
asmaa-elfatayry <asmaa.elfatayry22@gmail.com>
2024-07-15 02:30:00 +0300

    payment style

---
+++ aspnet-core/src/Event_Planning_System.EntityFrameworkCore/Migrations/20240704001853_FavoriteEventtable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Event_Planning_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class FavoriteEventtable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FavoriteEvents_AbpUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FavoriteEvents_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteEvents_EventId",
+                table: "FavoriteEvents",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteEvents_UserId",
+                table: "FavoriteEvents",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteEvents");
+        }
+    }
+}
