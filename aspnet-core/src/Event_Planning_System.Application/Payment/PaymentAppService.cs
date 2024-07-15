2c80c0b585467a8f68af2fc4e4c49563015bdab8
Nada Atef <nadaatefelsheshtawy@gmail.com>
2024-07-15 23:47:11 +0300

    fix

--- aspnet-core/src/Event_Planning_System.Application/Payment/PaymentAppService.cs
+++ aspnet-core/src/Event_Planning_System.Application/Payment/PaymentAppService.cs
@@ -12,79 +12,6 @@ using Abp.Domain.Repositories;
 using Event_Planning_System.Entities;
 using Event_Planning_System.Authorization.Users;
 using Event_Planning_System.Payment.Dto;
-
-//public class PaymentAppService : ApplicationService, IPaymentAppService
-//{
-//    private readonly StripeSettings _stripeSettings;
-//    private readonly ILogger<PaymentAppService> _logger;
-//    private readonly IRepository<Payment, int> _paymentrepository;
-//    private readonly IRepository<User, long> _userRepository;
-//    private readonly UserManager _userManager;
-//    private readonly IRepository<Event_Planning_System.Enitities.Event, int> _repositoryEvent;
-
-//    public PaymentAppService(
-//        IOptions<StripeSettings> stripeSettings,
-//        ILogger<PaymentAppService> logger,
-//        IRepository<Payment, int> paymentRepository,
-//        IRepository<Event_Planning_System.Enitities.Event, int> repositoryEvent,
-//        IRepository<User, long> userRepository,
-//        UserManager userManager)
-//    {
-//        _stripeSettings = stripeSettings.Value;
-//        _logger = logger;
-//        _paymentrepository = paymentRepository;
-//        _userRepository = userRepository;
-//        _userManager = userManager;
-//        _repositoryEvent = repositoryEvent;
-//        StripeConfiguration.ApiKey = "sk_test_51PcdCYAFPDLjCbDzmNaL6xyPq1hQLNTrNF6RapBsORBczDVcxnGeFyEjCxWjGO5SOKuSzIKUc8pUVKBSRF28LjTg002BalgpTa";
-//    }
-
-//    [HttpPost]
-//    public async Task<IActionResult> CreateCheckoutSession(string amount)
-//    {
-//        try
-//        {
-//            _logger.LogInformation("Starting CreateCheckoutSession with amount: {Amount}", amount);
-
-//            var options = new SessionCreateOptions
-//            {
-//                PaymentMethodTypes = new List<string> { "card" },
-//                LineItems = new List<SessionLineItemOptions>
-//                {
-//                    new SessionLineItemOptions
-//                    {
-//                        PriceData = new SessionLineItemPriceDataOptions
-//                        {
-//                            Currency = "gbp",
-//                            ProductData = new SessionLineItemPriceDataProductDataOptions
-//                            {
-//                                Name = "Event Ticket",
-//                            },
-//                            UnitAmount = int.Parse(amount),
-//                        },
-//                        Quantity = 1,
-//                    },
-//                },
-//                Mode = "payment",
-//                SuccessUrl = "http://localhost:4200/success",
-//                CancelUrl = "http://localhost:4200/cancel",
-//            };
-
-//            var service = new SessionService();
-//            Session session = await service.CreateAsync(options);
-
-//            _logger.LogInformation("Checkout session created successfully with ID: {SessionId}", session.Id);
-//            _logger.LogInformation("Checkout session details: {@Session}", session);
-//            return new OkObjectResult(new { sessionId = session.Id });
-//        }
-//        catch (Exception ex)
-//        {
-//            _logger.LogError(ex, "Error occurred while creating checkout session.");
-//            return new StatusCodeResult(500);
-//        }
-//    }
-//}
-
 public class PaymentAppService : ApplicationService, IPaymentAppService
 {
     private readonly StripeSettings _stripeSettings;
@@ -123,13 +50,6 @@ public class PaymentAppService : ApplicationService, IPaymentAppService
                paymentDto.GuestId=userId;
 
             }
-
-            //var eventPanner = _repositoryEvent.FirstOrDefault(a => a.Id == eventId);
-            //if (eventPanner != null)
-            //{
-            //    paymentDto.UserId =eventPanner.UserId ;
-            //}
-
 
             _logger.LogInformation("Starting CreateCheckoutSession with amount: {Amount}", paymentDto.Money);
 
