c2e7b0b02052bc90f6232cbbc641c7acbbd4a0a7
esraashabana <104281057+esraashabana@users.noreply.github.com>
2024-07-17 01:16:31 +0300

    addSupplierApis

---
+++ angular/src/app/supplier-places/supplier-places.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-places',
+  standalone: true,
+  imports: [],
+  templateUrl: './supplier-places.component.html',
+  styleUrl: './supplier-places.component.css'
+})
+export class SupplierPlacesComponent {
+
+}
